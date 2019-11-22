347cae76495d3f5fd200dac1da7822dbd7084777
AlexeyChur <44802663+AlexeyChur@users.noreply.github.com>
2019-11-22 17:12:13 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,34 @@
+
+
+const inputCalc = document.querySelector('.account'),
+    resultCalc = document.querySelector('.result');
+function input(i) {
+    inputCalc.value = inputCalc.value+i
+    
+}
+
+        function result() {
+
+                if (eval(inputCalc.value) ==undefined) {
+                    resultCalc.value = '0,00';
+          inputCalc.value = '0,00';
+
+                }
+                if (eval(inputCalc.value)==Infinity){
+                     resultCalc.value = '0,00';
+          inputCalc.value = 'Ярик так нельзя делать';
+                }
+
+            resultCalc.value = eval(inputCalc.value);
+          inputCalc.value = eval(inputCalc.value);
+            
+        }
+        function backspace() {
+            inputCalc.value = inputCalc.value.substring(0, inputCalc.value.length-1)
+            
+        }
+        function  reset() {
+
+         inputCalc.value = inputCalc.value.substring(0, inputCalc.value.length-1000000000000000)
+            
+        }
